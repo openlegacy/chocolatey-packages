2b44de4f187e714f8bb1279136d1a1c0d128a9f1
ol-test <admin@openlegacy.com>
2020-12-27 13:11:56 +0000

    Update to 1.17.0

---
+++ packages/openlegacy-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.17.0/windows/openlegacy-cli.zip' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'  # 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  url           = $url
+
+  softwareName  = 'openlegacy-cli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum      = '041596832b1360125c2fa68b93628de1d8fa0ea69480d00366107c7cc7d25cdf'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+}
+
+Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+Install-ChocolateyPath "$toolsDir/ol/bin" 'Machine' # Machine will assert administrative rights
+
+echo "*********************************************************************************************"
+echo "Note that the CLI require java 11, please make sure its installed and JAVA_HOME is configured"
+echo "*********************************************************************************************"
