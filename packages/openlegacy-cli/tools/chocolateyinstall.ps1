d82c4725f5f861d782dd8e0dc325c4efc5025e88
ol-test <admin@openlegacy.com>
2021-04-19 12:24:33 +0000

    Update to 1.32.0

--- packages/openlegacy-cli/tools/chocolateyinstall.ps1
+++ packages/openlegacy-cli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.31.0/windows/openlegacy-cli.zip' # download url, HTTPS preferred
+$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.32.0/windows/openlegacy-cli.zip' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,10 @@ $packageArgs = @{
 
   softwareName  = 'openlegacy-cli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '5b2cd86f70446182e7efd2eb45ea2dfc88065ae42d25dfc47022c7d4fd6f3944'
+  checksum      = 'db158347a841b29606310d1d6faec79b433cff78a41550cf5e5c8f71bfdc6010'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
+
 Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 Install-ChocolateyPath "$toolsDir\ol\bin" 'Machine' # Machine will assert administrative rights
 
