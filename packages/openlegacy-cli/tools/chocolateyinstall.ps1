1255dc60967942bdc8ff3174a76a3d7510c2371c
ol-test <admin@openlegacy.com>
2021-09-23 10:22:40 +0000

    Update to 1.39.2

--- packages/openlegacy-cli/tools/chocolateyinstall.ps1
+++ packages/openlegacy-cli/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'openlegacy-cli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = 'a3c5039a90f27042a00a92fd73a27736e3d2a8c2a9c951c4b0f47d778e567555'
+  checksum      = '512e64b37b0a8d07b0169b7047d5cfa98210c609e19ef4424d427b0e3264d8b9'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
