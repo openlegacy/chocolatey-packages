746db5a7e0d4f145706af13293096e5dca0ffffa
ol-test <admin@openlegacy.com>
2022-06-01 06:54:41 +0000

    Update to 1.52.0

--- packages/openlegacy-cli/tools/chocolateyinstall.ps1
+++ packages/openlegacy-cli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.51.1/windows/openlegacy-cli.zip' # download url, HTTPS preferred
+$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/1.52.0/windows/openlegacy-cli.zip' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'openlegacy-cli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = 'c3856f52fd737a4271176f3255c6990f4658697700aa313912de349ce11e669e'
+  checksum      = '105f8320260cdaa96c46e1bc8c02fdb520174560633a38a3707d10957d614ec1'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
@@ -31,3 +31,4 @@ foreach ($i in $dirnames) {
 echo "*******************************************************************************************************"
 echo "Note that the CLI require java 11 or higher, please make sure its installed and JAVA_HOME is configured"
 echo "*******************************************************************************************************"
+
