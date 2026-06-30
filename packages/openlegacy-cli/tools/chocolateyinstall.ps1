d5b0e41d448c96bddfb12fbd7014e930e8522bff
ol-test <admin@openlegacy.com>
2026-06-30 07:04:28 +0000

    Update to 3.0.33

--- packages/openlegacy-cli/tools/chocolateyinstall.ps1
+++ packages/openlegacy-cli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/3.0.32/windows/openlegacy-cli.zip' # download url, HTTPS preferred
+$url        = 'https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/3.0.33/windows/openlegacy-cli.zip' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'openlegacy-cli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '14796b957b2f193d71d556a8edc2c0e1f8f27cfcc690e815be986a649404c6fc'
+  checksum      = 'af14cbc328d5529cdc9bc1eb55fa655ea7fe54d56d47609b457d0664d8078625'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 }
 
